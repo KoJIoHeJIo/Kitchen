e28f37bbd4e332f14ad2f863c67ad947d6b717e9
frystylle <frystylle@yandex.ru>
2016-03-18 01:12:12 +0600

    Подборка подходящего дерева приложения.
    Тестирование и просмотр разных схем для поддержания необходимых функций.

--- app/src/main/java/com/example/kitchenv12/MainActivity.java
+++ app/src/main/java/com/example/kitchenv12/MainActivity.java
@@ -2,12 +2,48 @@ package com.example.kitchenv12;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.test.suitebuilder.annotation.SmallTest;
+import android.view.View;
+import android.app.Activity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Timer;
+import java.util.TimerTask;
+
 
 public class MainActivity extends AppCompatActivity {
+    int i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        final TextView tv = (TextView) findViewById(R.id.textView2);
+
+
+
+
     }
-}
+};
+
+
+
+
+
+
+
---
+++ app/src/main/java/i/layout/Main2Activity.java
@@ -0,0 +1,29 @@
+package i.layout;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+
+import com.example.kitchenv12.R;
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+
+
+
+    }
+    public void onClick (View view)
+    {
+
+    }
+
+
+}
