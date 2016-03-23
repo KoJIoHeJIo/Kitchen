ca6e783da9e523a013e0f4979ae42ee7ac30506f
frystylle <frystylle@yandex.ru>
2016-03-24 00:43:58 +0600

    Добавить функцию дополнения списка
    Добавлено тектовое поле для ввода, добавлена функция добавления записи в
    список.

--- app/src/main/java/i/layout/Main3Activity.java
+++ app/src/main/java/i/layout/Main3Activity.java
@@ -7,33 +7,52 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
 import com.example.kitchenv12.R;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class Main3Activity extends AppCompatActivity{
-
-    // Источник списка
-    String[] mSign = {"Хлеб белый", "Молоко 3.2% жирности", "Мясо", "Яйца битые", "Спички самовоспламеняющиеся", "Курица, грудка",
-            "Масло", "Халва", "Вино красное, полусладкое 1781 года", "Шашлык свинной", "Вода Норинга 1л", "Сайра в консерве"};
-
-
 
         @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-            String[] shoplist = getResources().getStringArray(R.array.shopping_list);
 
-        //Основной код списка
-        setContentView(R.layout.activity_main3);
+            // Источник списка
+            final  String[] member = getResources().getStringArray(R.array.shopping_list);
+            final ArrayList <String> shoplist = new ArrayList<String>();
+            shoplist.addAll(Arrays.asList(member));
+
+                    //Основной код списка
+                    setContentView(R.layout.activity_main3);
         ListView listView = (ListView) findViewById(R.id.listView);
+          final  EditText editText = (EditText) findViewById(R.id.editText);
+
 
 try {
-    ArrayAdapter<String> adapter = new ArrayAdapter<String>(Main3Activity.this, android.R.layout.simple_list_item_1, shoplist);
+    final ArrayAdapter<String> adapter = new ArrayAdapter<String>(Main3Activity.this, android.R.layout.simple_list_item_1, shoplist);
     listView.setAdapter(adapter);
+//Обработчик нажатия на клавишу энтер и добавления в список
+    editText.setOnKeyListener(new View.OnKeyListener() {
+        public boolean onKey(View v, int keyCode, KeyEvent event) {
+            //
+            if (event.getAction() == KeyEvent.ACTION_DOWN)
+                if (keyCode == KeyEvent.KEYCODE_ENTER) {
+                    shoplist.add(0, editText.getText().toString());
+                    adapter.notifyDataSetChanged();
+                    editText.setText("");
+                    return true;
+                }
+            return false;
+        }
+    });
 }
 catch (Exception e)
 { Toast toast = Toast.makeText(getApplicationContext(),
