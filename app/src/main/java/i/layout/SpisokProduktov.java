214b6cb158585f995e903633b9f38782672cc8ea
frystylle <frystylle@yandex.ru>
2016-04-17 17:30:13 +0600

    Начать разрабатывать список продуктов.
    Список будет содержать, меню из мест хранения продуктов.

---
+++ app/src/main/java/i/layout/SpisokProduktov.java
@@ -0,0 +1,10 @@
+package i.layout;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by Екатерина on 17.04.2016.
+ */
+public class SpisokProduktov extends AppCompatActivity {
+
+}
